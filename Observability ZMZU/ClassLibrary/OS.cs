8ce22dfab666fa5b4236ed806cfb1068feb69518
Кирякин Артём <a538434@mail.ru>
2025-11-30 16:25:33 +0700

    добавлен блок оценивания состояния

---
+++ Observability ZMZU/ClassLibrary/OS.cs
@@ -0,0 +1,56 @@
+﻿using ASTRALib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class OS
+    {
+        public static Dictionary<string, Dictionary<List<string>, int>> GoOS(List<int> listTelemetry, List<string> listSlices)
+        {
+            Dictionary<string, Dictionary<List<string>, int>> resultOS = new Dictionary<string, Dictionary<List<string>, int>> { };
+            foreach(string slice in listSlices)
+            {
+                DirectoryInfo directory = new DirectoryInfo(slice);
+                string targetFolder = directory.Parent.Name;
+                IRastr rastr = new Rastr();
+                rastr.Load(RG_KOD.RG_REPL, slice, "");
+                ITable tabelTI = (ITable)rastr.Tables.Item("ti");
+                ICol tiNumber = (ICol)tabelTI.Cols.Item("Num");
+                ICol name = (ICol)tabelTI.Cols.Item("name");
+                ICol status = (ICol)tabelTI.Cols.Item("sta");
+                int schet = tabelTI.Size;
+                List<int> listTINumbers = new List<int> { };
+                List<string> listTINames = new List<string> { };
+                for (int i = 0; i < schet; i++)
+                {
+                    listTINumbers.Add(Convert.ToInt32(tiNumber.get_ZN(i)));
+                    listTINames.Add(Convert.ToString(name.get_ZN(i)));
+                }
+                foreach(int telemetry in listTelemetry)
+                {
+                    if (listTINumbers.Contains(telemetry))
+                    {
+                        int index = listTINumbers.IndexOf(telemetry);
+                        status.set_ZN(index, true);
+                        var os = rastr.opf("s");
+                        if (resultOS.ContainsKey(targetFolder))
+                        {
+                            resultOS[targetFolder][new List<string> { Convert.ToString(name.get_ZN(index)), Convert.ToString(tiNumber.get_ZN(index)) }] = Convert.ToInt32(os);
+                        }
+                        else
+                        {
+                            resultOS[targetFolder] = new Dictionary<List<string>, int> { };
+                            resultOS[targetFolder][new List<string> { Convert.ToString(name.get_ZN(index)), Convert.ToString(tiNumber.get_ZN(index)) }] = Convert.ToInt32(os);
+                        }
+                    }
+                }
+            }
+            return resultOS;
+        }
+    }
+}
--- Observability ZMZU/Observability ZMZU/Program.cs
+++ Observability ZMZU/Observability ZMZU/Program.cs
@@ -7,16 +7,26 @@ namespace Observability_ZMZU
     {
         static void Main(string[] args)
         {
-            Dictionary<string, double> dict = CalculationObservability.CalculateObservability("D:\\Учеба\\Срезы", new DateTime(2023, 1, 11, 1, 0, 23), new DateTime(2023, 1, 11, 2, 23, 30), 1);
-            foreach (KeyValuePair<string, double> pair in dict)
-            {
-                Console.WriteLine($"{pair.Key}: {pair.Value}");
-            }
+            //Dictionary<string, double> dict = CalculationObservability.CalculateObservability("D:\\Учеба\\Срезы", new DateTime(2023, 1, 11, 1, 0, 23), new DateTime(2023, 1, 11, 2, 23, 30), 1);
+            //foreach (KeyValuePair<string, double> pair in dict)
+            //{
+            //    Console.WriteLine($"{pair.Key}: {pair.Value}");
+            //}
             //List<string> a = FileStorageConnection.GetRastrFiles("D:\\Учеба\\Срезы", new DateTime(2023, 1, 11, 1, 0, 23), new DateTime(2023, 1, 11, 14, 2, 3));
             //foreach(string i in a)
             //{
             //    Console.WriteLine(i);
             //}
+            Dictionary<string, Dictionary<List<string>, int>> resultOS = OS.GoOS(new List<int> {19967, 49004}, FileStorageConnection.GetRastrFiles("D:\\Учеба\\Срезы", new DateTime(2023, 1, 11, 1, 0, 23), new DateTime(2023, 1, 11, 2, 23, 30)));
+            foreach (var system in resultOS)
+            {
+                foreach (var nodesData in system.Value)
+                {
+                    string nodesString = string.Join(", ", nodesData.Key);
+
+                    Console.WriteLine($"│ {system.Key} │ {nodesString} │ {nodesData.Value} │");
+                }
+            }
             Console.ReadLine();
         }
     }
