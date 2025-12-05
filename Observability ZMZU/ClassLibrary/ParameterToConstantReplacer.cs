cb7aa1fd37194a28990a1b8ac180a852559128fb
Кирякин Артём <a538434@mail.ru>
2025-12-05 18:12:46 +0700

    Исправлдена функциональность

---
+++ Observability ZMZU/ClassLibrary/ParameterToConstantReplacer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ParameterToConstantReplacer : ExpressionVisitor
+    {
+        private readonly Dictionary<string, double> _values;
+
+        public ParameterToConstantReplacer(Dictionary<string, double> values)
+        {
+            _values = values;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (_values.TryGetValue(node.Name, out double val))
+                return Expression.Constant(val);
+
+            return base.VisitParameter(node);
+        }
+    }
+}
